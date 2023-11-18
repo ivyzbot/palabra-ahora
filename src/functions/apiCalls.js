6f8ce7da10c6ab0fcdb84421ecb7395b7c7907f8
ivyzhang <xiaowen_zhang@outlook.com>
2023-11-18 09:01:41 +0800

    v1.4

--- src/functions/apiCalls.js
+++ src/functions/apiCalls.js
@@ -1,13 +1,22 @@
 const AIRTABLE_BASE_URL = "https://api.airtable.com/v0/app9Ylybbbvc5URfR";
 const AIRTABLE_TOKEN = import.meta.env.VITE_AIRTABLE_TOKEN;
 
-async function fetchAirTableWords(setResult) {
-  const response = await fetch(`${AIRTABLE_BASE_URL}/main`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${AIRTABLE_TOKEN}`,
-    },
-  });
+const GIPHY_BASE_URL = "https://api.giphy.com/v1/gifs/search?";
+const GIPHY_CONFIG =
+  "limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+const GIPHY_TOKEN = import.meta.env.VITE_GIPHY_TOKEN;
+
+async function fetchAirTableWords(numWords, setResult) {
+  const response = await fetch(
+    `${AIRTABLE_BASE_URL}/main?pageSize=${numWords}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${AIRTABLE_TOKEN}`,
+      },
+    }
+  );
+
   const jsonData = await response.json();
   setResult(jsonData.records);
 }
@@ -25,4 +34,14 @@ async function updateAirTableWords(id, fields) {
   return response;
 }
 
-export { fetchAirTableWords, updateAirTableWords };
+async function fetchGiphy(word) {
+  const response = await fetch(
+    `${GIPHY_BASE_URL}api_key=${GIPHY_TOKEN}&q=${word}&${GIPHY_CONFIG}`
+  );
+  const jsonData = await response.json();
+  const url = jsonData.data[0].images.fixed_height.url;
+
+  return url;
+}
+
+export { fetchAirTableWords, updateAirTableWords, fetchGiphy };
